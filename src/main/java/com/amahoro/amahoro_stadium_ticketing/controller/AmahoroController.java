dffb13220ebd063ba70d5b25583a04badeb6e939
Muganza Igihozo Julie <167878527+JulieMuganza@users.noreply.github.com>
2024-12-23 12:15:08 +0200

    init commit

--- src/main/java/com/amahoro/amahoro_stadium_ticketing/controller/AmahoroController.java
+++ src/main/java/com/amahoro/amahoro_stadium_ticketing/controller/AmahoroController.java
@@ -24,7 +24,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class AmahoroController {
 
 //    @Autowired
